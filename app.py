175d8f1aecd7e5388d3eab6318934b7fc91081c8
Zachary Bush <zmbush@berkeley.edu>
2012-02-17 19:49:19 -0800

    Added logging

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
+import flask
+import logging
 
-from flask import Flask
-
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
@@ -14,8 +14,13 @@ def helloAndre():
 
 @app.route('/dropbox')
 def dropboxPage():
-  return render_template('dropbox.html', error=error)
+  print "boo"
+  returnVal = flask.render_template('dropbox.html', error=error)
+
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
+  logger = logging.FileHandler('logfiles')
+  logger.setLevel(logging.WARNING)
+  app.logger.addHandler(logger)
   app.run(host='0.0.0.0', port=port)
