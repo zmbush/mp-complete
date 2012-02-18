8c8eeab5e0a399b959a6548be1a0b2863030609d
Zachary Bush <zmbush@berkeley.edu>
2012-02-17 20:18:58 -0800

    Working on dropbox

---
+++ static/dropbox.js
@@ -0,0 +1,6 @@
+var dropbox = document.getElementByID('dropbox')
+
+dropbox.addEventListener("dragenter", dragEnter, false);
+dropbox.addEventListener("dragexit", dragExit, false);
+dropbox.addEventListener("dragover", dragOver, false);
+dropbox.addEventListener("drop", drop, false);
