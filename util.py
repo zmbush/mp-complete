aa1b4e3cfd8e5ca4ec100ee19351603b2a8d2301
Zachary Bush <zmbush@berkeley.edu>
2012-02-18 07:18:24 -0800

    Added pretty stuff to page

--- util.py
+++ util.py
@@ -40,8 +40,9 @@ except ImportError:
 # MusixMatch API key, should be an environment variable
 MUSIXMATCH_API_KEY = None
 if('MUSIXMATCH_API_KEY' in os.environ):
-    print "boo"
-    MUSIXMATCH_API_KEY = os.environ['MUSIXMATCH_API_KEY']
+  MUSIXMATCH_API_KEY = os.environ['MUSIXMATCH_API_KEY']
+else:
+  MUSIXMATCH_API_KEY = 'd4511e93df6c32de7bae6393025550c5'
 
 # details of the website to call
 API_HOST = 'api.musixmatch.com'
