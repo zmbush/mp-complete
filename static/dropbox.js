ae75a55fae112a14c61ab765355d98bd1090a9ea
Zachary Bush <zmbush@berkeley.edu>
2012-02-17 20:57:17 -0800

    Working on dropbox

--- static/dropbox.js
+++ static/dropbox.js
@@ -1,6 +1,41 @@
-var dropbox = document.getElementByID('dropbox')
+function hookDropbox(){
+  var dropbox = document.getElementById('dropbox')
 
-dropbox.addEventListener("dragenter", dragEnter, false);
-dropbox.addEventListener("dragexit", dragExit, false);
-dropbox.addEventListener("dragover", dragOver, false);
-dropbox.addEventListener("drop", drop, false);
+  dropbox.addEventListener("dragenter", noopHandler, false);
+  dropbox.addEventListener("dragexit", noopHandler, false);
+  dropbox.addEventListener("dragover", noopHandler, false);
+  dropbox.addEventListener("drop", drop, false);
+}
+
+function noopHandler(evt){
+  evt.stopPropagation();
+  evt.preventDefault();
+}
+
+function drop(evt){
+  evt.stopPropagation();
+  evt.preventDefault();
+
+  var files = evt.dataTransfer.files;
+  var count = files.length
+
+  if (count > 0)
+    handleFiles(files)
+}
+
+function handleFiles(files){
+  for(var i = 0; i < files.length; i++){
+    file = files[i]
+    var reader = new FileReader();
+     
+     // init the reader event handlers
+     reader.onload = handleReaderLoad;
+     // begin the read operation
+     reader.readAsDataURL(file);
+  }
+}
+
+function handleReaderLoad(evt){
+
+  alert(evt.target.result);
+}
