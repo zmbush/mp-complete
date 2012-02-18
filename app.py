9c3e18c3cfa833286f3c4dc8f0699779cdf82d41
Zachary Bush <zmbush@berkeley.edu>
2012-02-17 19:03:08 -0800

    minor Change

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ def hello():
 def helloAndre():
   return "Hello Andre!"
 
+@app.route('/dropbox')
+def dropboxPage():
+  retval = "<html>"
+  retval += "</html>"
+  return retval
+
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port)
